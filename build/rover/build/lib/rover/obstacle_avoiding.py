339048f65e01e9bfe79247b29b60f0ba53b56239
Rithwik-Ravi <mailrittyplay@gmail.com>
2025-11-08 00:04:57 +0530

    Updated obstacle avoidance logic to make turns dynamic V1

--- build/rover/build/lib/rover/obstacle_avoiding.py
+++ build/rover/build/lib/rover/obstacle_avoiding.py
@@ -23,54 +23,64 @@ class ObstacleAvoidingBot(Node):
         #subscriber
         self.subscription=self.create_subscription(LaserScan,'/scan',self.get_scan_values,40)
         #periodic publisher call
-        timer_period = 0.2;self.timer = self.create_timer(timer_period, self.send_cmd_vel)
+        timer_period = 0.1;self.timer = self.create_timer(timer_period, self.send_cmd_vel)
         ## Initializing Global values
         ## given a value for VELOCITY
         self.linear_vel = 0.22 
         ## Making dictionary to divide the area of lase scan 
-        self.regions={'right':[],'mid':[],'left':[]}
+        self.regions = {
+            'r1': 100.0, 'r2': 100.0, 'r3': 100.0,
+            'r4': 100.0, 'r5': 100.0, 'r6': 100.0,
+            'r7': 100.0, 'r8': 100.0, 'r9': 100.0,
+            'r10': 100.0, 'r11': 100.0, 'r12': 100.0
+        }
         ## creating a message object to fit new velocities and publish them
         self.velocity=Twist()
 
 
     ## Subscriber Callback function 
     def get_scan_values(self,scan_data):
-        ## We have 360 data points and we are dividing them in 3 regions
-        ## we say if there is something in the region get the smallest value
-        self.regions = {
-        'right':   int(min(min(scan_data.ranges[120:160])  , 100)),
-        'mid':     int(min(min(scan_data.ranges[160:200]), 100)),
-        'left':    int(min(min(scan_data.ranges[200:240]), 100)),
-        'back':    int(min(min(scan_data.ranges[360:720])   , 100))
-        }
-        print(self.regions['left']," / ",self.regions['mid']," / ",self.regions['right'],self.regions['back'])
+        # We have a 120-degree frontal view, divided into 12 regions of 10 degrees each.
+        # LaserScan data is 360 points. We use indices 120 to 240.
+        # region 'r1' is the furthest right, 'r12' is the furthest left.
+        num_regions = 12
+        for i in range(num_regions):
+            start_angle = 120 + i * 10
+            end_angle = start_angle + 10
+            # We cap the minimum distance at 100 to avoid potential issues with large values.
+            self.regions[f'r{i+1}'] = min(min(scan_data.ranges[start_angle:end_angle]), 100.0)
 
   
     ## Callback Publisher of velocities called every 0.2 seconds
     def send_cmd_vel(self):
-        ## angular and linear velocities are set into object self.velcity
-        ## setting the linear velocity to be fixed and robot will keep on moving
-        self.velocity.linear.x=self.linear_vel
-        ## cases to make the robot change its angular velocity
-        if(self.regions['left'] > 1.5  and self.regions['mid'] > 1.5   and self.regions['right'] > 1.5 ):
-            self.velocity.angular.z=0.0 # condition in which area is total clear
-            print("forward")
-        elif(self.regions['left'] > 1.5 and self.regions['mid'] > 1.5  and self.regions['right'] < 1.5 ):
-            self.velocity.angular.z=-0.8# object on right,taking  left 
-            self.velocity.linear.x=0.2 # if there is something on right, slow down
-            print("right")
-        elif(self.regions['left'] < 1.5 and self.regions['mid'] > 1.5  and self.regions['right'] > 1.5 ):
-            self.velocity.angular.z=0.8 #object  on left, taking  right
-            self.velocity.linear.x=0.2 # if there is something on left, slow down
-            print("left")          
-        elif(self.regions['left'] < 1.5 and self.regions['mid'] < 1.5  and self.regions['right'] < 1.5 ):
-            self.velocity.linear.x=0.0
-            self.velocity.angular.z=1.57# object ahead take full turn
-            print("Full Turn")
-        elif(self.regions['back'] < 1.5 ):
-            self.velocity.linear.x=0.0 # if there is something behind stop
-        else:## lThis code is not completed ->  you have  to add more conditions  ot make it robust
-            print("some other conditions are required to be programmed") 
+        # Proportional control for steering
+        # Weights for regions: regions closer to the center have a higher impact on turning.
+        # The regions are r1 (rightmost) to r12 (leftmost).
+        # A positive weight will result in a positive angular.z (left turn).
+        # A negative weight will result in a negative angular.z (right turn).
+        weights = [1.3, 1.1, 0.9, 0.7, 0.5, 0.3, -0.3, -0.5, -0.7, -0.9, -1.1, -1.3]
+        
+        obstacle_threshold = 1.5
+        angular_velocity = 0.0
+        linear_velocity = self.linear_vel
+        
+        obstacle_detected = False
+        for i in range(len(weights)):
+            region_dist = self.regions[f'r{i+1}']
+            if region_dist < obstacle_threshold:
+                obstacle_detected = True
+                # The closer the obstacle, the stronger the turn
+                turn_strength = (obstacle_threshold - region_dist) / obstacle_threshold
+                angular_velocity += weights[i] * turn_strength
+
+        # If front regions detect a close obstacle, slow down and turn more sharply
+        if self.regions['r6'] < 0.8 or self.regions['r7'] < 0.8:
+            linear_velocity = 0.1 # Slow down for safety
+            print("Obstacle ahead, slowing down.")
+
+        # If no obstacles are detected, move straight. Otherwise, use calculated velocities.
+        self.velocity.linear.x = linear_velocity if obstacle_detected else self.linear_vel
+        self.velocity.angular.z = angular_velocity
        
         ## lets publish the complete velocity
         self.publisher.publish(self.velocity)
